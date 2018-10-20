113112055e7a639cb1ad28e7e0551c8ad2ab448a
robin9421 <prajapatirobin123@gmail.com>
2018-10-20 15:36:47 +0530

    first commit

---
+++ js/img.js
@@ -0,0 +1,15 @@
+// var myImage = document.getElementById("img");
+// var imageArray = ["images/2.png", "images/3.png", "images/4.png", "images/1.png"];
+// var imageIndex = 0;
+
+// function changeImage() {
+//     myImage.setAttribute("src", imageArray[imageIndex]);
+//     imageIndex++;
+//     if (imageIndex >= imageArray.length) {
+//         imageIndex = 0;
+
+//     }
+// }
+
+
+// setInterval(changeImage,5000); 
---
+++ js/slide.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#slide").click(function(){
+        $(".fh5co-tab-menu").slideToggle("slow");
+    });
+});
